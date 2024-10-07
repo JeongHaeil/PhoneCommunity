ba7e04f6e7390d1d50ec0e2b1695e78b1d408f8e
tosmr <tosmr@tosmreo>
2024-10-08 00:22:51 +0900

    PasswordEncoder 이걸로 다 바꿨는데 안돼요 .. ㅠ

--- final/src/main/java/xyz/itwill/controller/UserController.java
+++ final/src/main/java/xyz/itwill/controller/UserController.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
